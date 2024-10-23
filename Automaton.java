919f4ffe542838d98d3e751757026b0c23d8c542
Adrian-Utz <ajutz2003@gmail.com>
2024-10-23 16:51:26 -0400

    Add files via upload
    Added the base program

---
+++ Automaton.java
@@ -0,0 +1,15 @@
+public abstract class Automaton {
+    
+    protected GridCanvas grid;
+    
+    public abstract void update();
+
+    public void run(String title, int delay) {
+    // body of this method omitted
+    }
+
+    private void mainloop(int rate) {
+    // body of this method omitted
+    }
+    
+}
